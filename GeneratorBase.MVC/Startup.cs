8d2141a49ae50d88a0a47f3660ea48a56fa5c98d
mahesh.sharma@etelic.com <mahesh.sharma@etelic.com>
2022-07-20 18:39:19 +0530

    'message'

--- GeneratorBase.MVC/Startup.cs
+++ GeneratorBase.MVC/Startup.cs
@@ -11,7 +11,7 @@ public partial class Startup
 {
     public void Configuration(IAppBuilder app)
     {
-		//mahesh test demo to mukul test
+		//mahesh test demo to mukul tetetetetet
         var allDbContextsTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.BaseType == (typeof(DbContext))).ToList();
         foreach(Type dbContextType in allDbContextsTypes)
         {
